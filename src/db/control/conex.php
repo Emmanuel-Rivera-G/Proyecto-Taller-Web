e9165905e4db716bf2399b0b5f5b19a72a8c13af
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-05-21 09:45:34 -0500

    Añadir: Control de la base de datos

---
+++ src/db/control/conex.php
@@ -0,0 +1,3 @@
+<?php
+$_conex = mysqli_connect("localhost","root", "", "db_usuario",3307);
+?>
---
+++ src/db/control/validacion.php
@@ -0,0 +1,43 @@
+<?php
+    include('../control/conex.php');
+    session_start();
+
+    if (isset($_POST['Usuario']) && isset($_POST['Contraseña'])) {
+        $_usuario = trim($_POST['Usuario']);
+        $_password = trim($_POST['Contraseña']);
+
+        $_SESSION['Usuario'] = $_usuario;
+
+        $_consulta = "SELECT * FROM usuario WHERE nombreUsuario='$_usuario' AND contraseñaUsuario='$_password'";
+        $_resultado = mysqli_query($_conex, $_consulta);
+
+        //esta variable sirve para almacenar el resultado
+        $_filas = mysqli_fetch_array($_resultado);
+
+        if ($_filas) {
+            if($_filas['codigo_tipoUsuario']==1)//Administrador
+            {
+                header("location: ../assets/prueba/pageAdmin.php");   //link a la pagina del administrador
+            }
+            else if($_filas['codigo_tipoUsuario']==2){ //cliente
+                header("location: ../assets/prueba/pageEmpleado.php");   //link a la pagina del Empleado1
+            }
+        } else {
+            include('../templates/login.html');
+            echo '
+            <div style="display: flex; justify-content: center;">
+            <h2>Error en la autenticacion</h2>
+            </div>';
+        }
+
+        //para mostrar el resultado
+        mysqli_free_result($_resultado);
+        mysqli_close($_conex);
+    } else {
+        include('../templates/login.html');
+        echo '
+        <div style="display: flex; justify-content: center;">
+        <h2>Por favor, ingrese su usuario y contraseña</h2>
+        </div>';
+    }
+?>
