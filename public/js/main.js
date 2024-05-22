9ba1a16e07698e1c9e775552cdd30212213d8b91
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-05-21 21:33:10 -0500

    Funciones para botones menu

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,40 @@ document.querySelector('#btn-menu').addEventListener('click', function () {
         menu.setAttribute('hidden', '');
     }
 });
+const usuarios = document.querySelector('#usuarios');
+const planillas = document.querySelector('#planillas');
+const contratos = document.querySelector('#contratos');
+
+document.querySelector('.btn-usuario').addEventListener('click', function () {
+    if (usuarios.hasAttribute('hidden')) {
+        usuarios.removeAttribute('hidden');
+        planillas.setAttribute('hidden', '');
+        contratos.setAttribute('hidden', '');
+    } else {
+        if (!planillas.hasAttribute('hidden') && !contratos.hasAttribute('hidden')) {
+            usuarios.setAttribute('hidden', '');
+        }
+    }
+});
+document.querySelector('.btn-planilla').addEventListener('click', function () {
+    if (planillas.hasAttribute('hidden')) {
+        planillas.removeAttribute('hidden');
+        usuarios.setAttribute('hidden', '');
+        contratos.setAttribute('hidden', '');
+    } else {
+        if (!usuarios.hasAttribute('hidden') && !contratos.hasAttribute('hidden')) {
+            planillas.setAttribute('hidden', '');
+        }
+    }
+});
+document.querySelector('.btn-contrato').addEventListener('click', function () {
+    if (contratos.hasAttribute('hidden')) {
+        contratos.removeAttribute('hidden');
+        usuarios.setAttribute('hidden', '');
+        planillas.setAttribute('hidden', '');
+    } else {
+        if (!planillas.hasAttribute('hidden') && !usuarios.hasAttribute('hidden')) {
+            contratos.setAttribute('hidden', '');
+        }
+    }
+});
