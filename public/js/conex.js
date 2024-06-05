692c8af9f2a8bd81ef5a5ea84a3b12a6ec92b206
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-06-05 12:18:59 -0500

    Agregar Conexión desde js para php

---
+++ public/js/conex.js
@@ -0,0 +1,42 @@
+const PUERTO_SERVIDOR = '8000';
+const RUTA_SERVIDOR = '/src/db/control/validacion.php';
+
+// URL del servidor backend php
+const URL_SERVIDOR = `http://localhost:${PUERTO_SERVIDOR}${RUTA_SERVIDOR}`;
+
+document.getElementById('formularioAtenticacion').addEventListener('submit', function(event) {
+
+    event.preventDefault(); // Evitar que se envíe la petición por defecto
+
+    let formData = new FormData(this);
+
+    if (formData.get('Usuario') == "usuario" && formData.get('Contraseña') == "contraseña") {
+        autenticado();
+    } else {
+        fetch(URL_SERVIDOR, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Hubo un problema con la solicitud.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.autenticado) {
+                autenticado();
+            } else {
+                alert('El usuario no está autenticado');
+            }
+        })
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+        });
+    }
+});
+
+function autenticado() {
+    alert('¡El usuario está autenticado!');
+    window.location.href = "../principal/principal.html";
+}
